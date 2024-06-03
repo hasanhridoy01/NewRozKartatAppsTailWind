af08d272bdd6bda099ee50ad12267edf76ed7350
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-06-03 18:50:15 +0600

    gift card added done

--- src/pages/Home/GiftCard/GiftCard.jsx
+++ src/pages/Home/GiftCard/GiftCard.jsx
@@ -1,3 +1,11 @@
+import { Button, Divider, Icon, Stack, Typography } from "@mui/material";
+import img1 from "../../../assets/Images/GiftCard/card.png";
+import img2 from "../../../assets/Images/GiftCard/card1.png";
+import img3 from "../../../assets/Images/GiftCard/card2.png";
+import img4 from "../../../assets/Images/GiftCard/card3.png";
+import PublicIcon from "@mui/icons-material/Public";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
 const GiftCard = () => {
   return (
     <div className="container mx-auto px-10">
@@ -24,6 +32,293 @@ const GiftCard = () => {
         >
           View All
         </button>
+      </div>
+
+      <div className="grid lg:grid-cols-12 md:grid-cols-6 sm:grid-cols-3 gap-4">
+        <div
+          className="col-span-3"
+          style={{
+            height: "100%",
+            border: "1px solid #E5E5E5",
+            padding: "12px",
+            gap: "12px",
+            background: "#FFFFFF",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <img src={img1} alt="" />
+          <p
+            className="mt-1"
+            style={{
+              fontWeight: 500,
+              fontSize: "16px",
+              lineHeight: "19px",
+              textAlign: "left",
+              width: "308px",
+            }}
+          >
+            Play station Network Card Global
+          </p>
+          <div className="flex" style={{ alignItems: "center", color: "gray" }}>
+            <Icon
+              component={PublicIcon}
+              fontSize="small"
+              sx={{ verticalAlign: "middle", marginRight: "8px" }}
+            />{" "}
+            <p>Global - Xbox</p>
+          </div>
+          <Divider />
+
+          <div className="flex justify-between w-full">
+            <div className="" style={{ paddingLeft: "0px", marginLeft: "0px" }}>
+              <Typography gutterBottom sx={{ fontSize: "11px" }}>
+                IQD 2405678.21
+              </Typography>
+              <Typography
+                gutterBottom
+                sx={{ fontSize: "13px", color: "#EC5928" }}
+              >
+                IQA 22622177.36
+              </Typography>
+            </div>
+            <div
+              className=""
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <ShoppingCartOutlinedIcon
+                sx={{
+                  fontSize: "40px",
+                  color: "#000",
+                  backgroundColor: "#fff",
+                  padding: "6px",
+                  marginRight: "10px",
+                  cursor: "pointer",
+                }}
+                size="small"
+              />
+              <Button variant="outlined" size="small">
+                Buy Now
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-span-3"
+          style={{
+            height: "100%",
+            border: "1px solid #E5E5E5",
+            padding: "12px",
+            gap: "12px",
+            background: "#FFFFFF",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <img src={img2} alt="" />
+          <p
+            className="mt-1"
+            style={{
+              fontWeight: 500,
+              fontSize: "16px",
+              lineHeight: "19px",
+              textAlign: "left",
+              width: "308px",
+            }}
+          >
+            Play station Network Card Global
+          </p>
+          <div className="flex" style={{ alignItems: "center", color: "gray" }}>
+            <Icon
+              component={PublicIcon}
+              fontSize="small"
+              sx={{ verticalAlign: "middle", marginRight: "8px" }}
+            />{" "}
+            <p>Global - Xbox</p>
+          </div>
+          <Divider />
+
+          <div className="flex justify-between w-full">
+            <div className="" style={{ paddingLeft: "0px", marginLeft: "0px" }}>
+              <Typography gutterBottom sx={{ fontSize: "11px" }}>
+                IQD 2405678.21
+              </Typography>
+              <Typography
+                gutterBottom
+                sx={{ fontSize: "13px", color: "#EC5928" }}
+              >
+                IQA 22622177.36
+              </Typography>
+            </div>
+            <div
+              className=""
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <ShoppingCartOutlinedIcon
+                sx={{
+                  fontSize: "40px",
+                  color: "#000",
+                  backgroundColor: "#fff",
+                  padding: "6px",
+                  marginRight: "10px",
+                  cursor: "pointer",
+                }}
+                size="small"
+              />
+              <Button variant="outlined" size="small">
+                Buy Now
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-span-3"
+          style={{
+            height: "100%",
+            border: "1px solid #E5E5E5",
+            padding: "12px",
+            gap: "12px",
+            background: "#FFFFFF",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <img src={img3} alt="" />
+          <p
+            className="mt-1"
+            style={{
+              fontWeight: 500,
+              fontSize: "16px",
+              lineHeight: "19px",
+              textAlign: "left",
+              width: "308px",
+            }}
+          >
+            Play station Network Card Global
+          </p>
+          <div className="flex" style={{ alignItems: "center", color: "gray" }}>
+            <Icon
+              component={PublicIcon}
+              fontSize="small"
+              sx={{ verticalAlign: "middle", marginRight: "8px" }}
+            />{" "}
+            <p>Global - Xbox</p>
+          </div>
+          <Divider />
+
+          <div className="flex justify-between w-full">
+            <div className="" style={{ paddingLeft: "0px", marginLeft: "0px" }}>
+              <Typography gutterBottom sx={{ fontSize: "11px" }}>
+                IQD 2405678.21
+              </Typography>
+              <Typography
+                gutterBottom
+                sx={{ fontSize: "13px", color: "#EC5928" }}
+              >
+                IQA 22622177.36
+              </Typography>
+            </div>
+            <div
+              className=""
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <ShoppingCartOutlinedIcon
+                sx={{
+                  fontSize: "40px",
+                  color: "#000",
+                  backgroundColor: "#fff",
+                  padding: "6px",
+                  marginRight: "10px",
+                  cursor: "pointer",
+                }}
+                size="small"
+              />
+              <Button variant="outlined" size="small">
+                Buy Now
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-span-3"
+          style={{
+            height: "100%",
+            border: "1px solid #E5E5E5",
+            padding: "12px",
+            gap: "12px",
+            background: "#FFFFFF",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <img src={img4} alt="" />
+          <p
+            className="mt-1"
+            style={{
+              fontWeight: 500,
+              fontSize: "16px",
+              lineHeight: "19px",
+              textAlign: "left",
+              width: "308px",
+            }}
+          >
+            Play station Network Card Global
+          </p>
+          <div className="flex" style={{ alignItems: "center", color: "gray" }}>
+            <Icon
+              component={PublicIcon}
+              fontSize="small"
+              sx={{ verticalAlign: "middle", marginRight: "8px" }}
+            />{" "}
+            <p>Global - Xbox</p>
+          </div>
+          <Divider />
+
+          <div className="flex justify-between w-full">
+            <div className="" style={{ paddingLeft: "0px", marginLeft: "0px" }}>
+              <Typography gutterBottom sx={{ fontSize: "11px" }}>
+                IQD 2405678.21
+              </Typography>
+              <Typography
+                gutterBottom
+                sx={{ fontSize: "13px", color: "#EC5928" }}
+              >
+                IQA 22622177.36
+              </Typography>
+            </div>
+            <div
+              className=""
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <ShoppingCartOutlinedIcon
+                sx={{
+                  fontSize: "40px",
+                  color: "#000",
+                  backgroundColor: "#fff",
+                  padding: "6px",
+                  marginRight: "10px",
+                  cursor: "pointer",
+                }}
+                size="small"
+              />
+              <Button variant="outlined" size="small">
+                Buy Now
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
